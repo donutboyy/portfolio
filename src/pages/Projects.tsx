f672847389744a2099d668a21f3e6c2793ca8abb
donutboyy <dndanielng929@gmail.com>
2024-09-02 23:31:16 +0800

    style: align images vertically in project carousel

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,46 +1,48 @@
-import { Typography, Card } from "@mui/material";
+import { Typography, Grid } from "@mui/material";
 import ProjectCard from "../components/ProjectCard";
 
 function Projects() {
   return (
     <>
-      <Typography id="#projects" variant="h4" align="center">
+      <Typography id="#projects" variant="h2" align="center">
         Projects
       </Typography>
-      <Card variant="outlined" sx={{ m: 2 }}>
-        <ProjectCard
-          name="Goblin deez Knights"
-          description="2D top down endless shooter made using the Unity Game Engine within a week"
-          images={[
-            {
-              src: "/images/gdk_title.png",
-            },
-            {
-              src: "/images/gdk_sc1.jpg",
-            },
-            {
-              src: "/images/gdk_sc2.jpg",
-            },
-          ]}
-        />
-      </Card>
-      <Card variant="outlined" sx={{ m: 2 }}>
-        <ProjectCard
-          name="tictactAI"
-          description="Tic Tac Toe game in the terminal with an unbeatable 'AI' made using minimax algorithm"
-          images={[
-            {
-              src: "/images/tictactai_sc1.png",
-            },
-            {
-              src: "/images/tictactai_sc2.png",
-            },
-            {
-              src: "/images/tictactai_sc3.png",
-            },
-          ]}
-        />
-      </Card>
+      <Grid container justifyContent="center" alignItems="center">
+        <Grid item display="flex" justifyContent="center" alignItems="center">
+          <ProjectCard
+            name="Goblin deez Knights"
+            description="2D top down endless shooter made using the Unity Game Engine within a week"
+            images={[
+              {
+                src: "/images/gdk_title.png",
+              },
+              {
+                src: "/images/gdk_sc1.jpg",
+              },
+              {
+                src: "/images/gdk_sc2.jpg",
+              },
+            ]}
+          />
+        </Grid>
+        <Grid item display="flex" justifyContent="center" alignItems="center">
+          <ProjectCard
+            name="tictactAI"
+            description="Tic Tac Toe game in the terminal with an unbeatable 'AI' made using minimax algorithm"
+            images={[
+              {
+                src: "/images/tictactai_sc1.png",
+              },
+              {
+                src: "/images/tictactai_sc2.png",
+              },
+              {
+                src: "/images/tictactai_sc3.png",
+              },
+            ]}
+          />
+        </Grid>
+      </Grid>
     </>
   );
 }
