a8f94da0eaeac0956604a0e07e05a4a449176c76
donutboyy <dndanielng929@gmail.com>
2026-04-14 17:53:40 +0800

    feat: combine hero and about me

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -15,7 +15,7 @@ import Brightness4Icon from "@mui/icons-material/Brightness4";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
 import { useThemeMode } from "../hooks/useThemeMode";
 
-const pages = ["About", "Projects"];
+const pages = ["Projects"];
 
 function ResponsiveAppBar() {
   const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,10 +1,11 @@
 import {
   Box,
-  Button,
   CssBaseline,
   responsiveFontSizes,
   ThemeProvider,
   Typography,
+  Stack,
+  SvgIcon,
 } from "@mui/material";
 import "./Root.css";
 import { getTheme } from "../theme.ts";
@@ -13,7 +14,6 @@ import { ThemeProvider as CustomThemeProvider } from "../context/ThemeProvider.t
 import ResponsiveAppBar from "../components/AppBar.tsx";
 import Projects from "../pages/Projects.tsx";
 import Footer from "../pages/Footer.tsx";
-import AboutMe from "../pages/AboutMe.tsx";
 import KeyboardDoubleArrowDownIcon from "@mui/icons-material/KeyboardDoubleArrowDown";
 import { useMemo } from "react";
 
@@ -27,34 +27,62 @@ function RootContent() {
       <ResponsiveAppBar />
       <Box className="hero">
         <div className="hero-content">
-          <Typography
-            variant="h1"
-            align="center"
-            className="hero-title"
-            sx={{ px: "10vw" }}
+          <Stack
+            direction={{ xs: "column", md: "row" }}
+            spacing={{ xs: "2vh", md: "5vh" }}
+            alignItems="center"
+            justifyContent="center"
           >
-            {"Hi, I am Daniel"}
-          </Typography>
-          <Typography
-            id="typewriter"
-            variant="h4"
-            align="center"
-            className="hero-subtitle"
-          >
-            I love creating meaningful software.
-          </Typography>
-          <div>
-            <a href="#about">
-              <Button variant="outlined" className="hero-button">
-                View my work{" "}
-                <KeyboardDoubleArrowDownIcon className="hero-arrow" />
-              </Button>
-            </a>
+            <div>
+              <div className="knight-container">
+                <img
+                  id="knight"
+                  src="data:image/svg+xml;charset=utf8,%3Csvg%20xmlns=%27http://www.w3.org/2000/svg%27%3E%3C/svg%3E"
+                  alt="Knight character"
+                />
+              </div>
+            </div>
+            <Stack
+              spacing={{ xs: "2vh", md: "5vh" }}
+              alignItems="center"
+              justifyContent="center"
+              sx={{ height: { xs: "auto", md: "80vh" } }}
+            >
+              <Box sx={{ display: "flex" }}>
+                <Typography
+                  variant="h1"
+                  align="center"
+                  className="hero-title"
+                  sx={{ width: { xs: "90vw", md: "50vw" } }}
+                >
+                  {"Hi, I am Daniel"}
+                </Typography>
+              </Box>
+              <div>
+                <Typography
+                  variant="h6"
+                  align="center"
+                  className="description"
+                  sx={{ width: { xs: "90vw", md: "50vw" } }}
+                >
+                  Software Engineer with experience in web, frontend, backend
+                  and game development.
+                </Typography>
+              </div>
+            </Stack>
+          </Stack>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <SvgIcon
+              component={KeyboardDoubleArrowDownIcon}
+              className="scroll-indicator"
+              sx={{
+                width: "10vh",
+                height: "20vh",
+                p: "1vh",
+              }}
+            />
           </div>
         </div>
-      </Box>
-      <Box id="about" className="section about-section">
-        <AboutMe />
       </Box>
       <Box
         id="projects"
