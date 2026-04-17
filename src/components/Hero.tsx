2e73fc33c8de6f54bf12f7bf838ec7ed4d062fcd
donutboyy <dndanielng929@gmail.com>
2026-04-17 12:22:01 +0800

    refactor: move hero to new component

---
+++ src/components/Hero.tsx
@@ -0,0 +1,69 @@
+import { Box, Stack, SvgIcon, Typography } from "@mui/material";
+import KeyboardDoubleArrowDownIcon from "@mui/icons-material/KeyboardDoubleArrowDown";
+import "./Hero.css";
+
+function Hero() {
+  return (
+    <Box className="hero">
+      <div className="hero-content">
+        <Stack
+          direction={{ xs: "column", md: "row" }}
+          spacing={{ xs: "2vh", md: "5vh" }}
+          alignItems="center"
+          justifyContent="center"
+        >
+          <div>
+            <div className="knight-container">
+              <img
+                id="knight"
+                src="data:image/svg+xml;charset=utf8,%3Csvg%20xmlns=%27http://www.w3.org/2000/svg%27%3E%3C/svg%3E"
+                alt="Knight character"
+              />
+            </div>
+          </div>
+          <Stack
+            spacing={{ xs: "2vh", md: "5vh" }}
+            alignItems="center"
+            justifyContent="center"
+            sx={{ height: { xs: "auto", md: "80vh" } }}
+          >
+            <Box sx={{ display: "flex" }}>
+              <Typography
+                variant="h1"
+                align="left"
+                className="hero-title"
+                sx={{ width: { xs: "90vw", md: "50vw" } }}
+              >
+                {"Hi, I am Daniel"}
+              </Typography>
+            </Box>
+            <div>
+              <Typography
+                variant="h6"
+                align="left"
+                className="description"
+                sx={{ width: { xs: "90vw", md: "50vw" } }}
+              >
+                Software Engineer with experience in web, frontend, backend and
+                game development.
+              </Typography>
+            </div>
+          </Stack>
+        </Stack>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <SvgIcon
+            component={KeyboardDoubleArrowDownIcon}
+            className="scroll-indicator"
+            sx={{
+              width: "10vh",
+              height: "20vh",
+              p: "1vh",
+            }}
+          />
+        </div>
+      </div>
+    </Box>
+  );
+}
+
+export default Hero;
--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -3,18 +3,15 @@ import {
   CssBaseline,
   responsiveFontSizes,
   ThemeProvider,
-  Typography,
-  Stack,
-  SvgIcon,
 } from "@mui/material";
 import "./Root.css";
 import { getTheme } from "../theme.ts";
 import { useThemeMode } from "../hooks/useThemeMode.tsx";
 import { ThemeProvider as CustomThemeProvider } from "../context/ThemeProvider.tsx";
 import ResponsiveAppBar from "../components/AppBar.tsx";
+import Hero from "../components/Hero.tsx";
 import Projects from "../pages/Projects.tsx";
 import Footer from "../pages/Footer.tsx";
-import KeyboardDoubleArrowDownIcon from "@mui/icons-material/KeyboardDoubleArrowDown";
 import { useMemo } from "react";
 
 function RootContent() {
@@ -25,65 +22,7 @@ function RootContent() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <ResponsiveAppBar />
-      <Box className="hero">
-        <div className="hero-content">
-          <Stack
-            direction={{ xs: "column", md: "row" }}
-            spacing={{ xs: "2vh", md: "5vh" }}
-            alignItems="center"
-            justifyContent="center"
-          >
-            <div>
-              <div className="knight-container">
-                <img
-                  id="knight"
-                  src="data:image/svg+xml;charset=utf8,%3Csvg%20xmlns=%27http://www.w3.org/2000/svg%27%3E%3C/svg%3E"
-                  alt="Knight character"
-                />
-              </div>
-            </div>
-            <Stack
-              spacing={{ xs: "2vh", md: "5vh" }}
-              alignItems="center"
-              justifyContent="center"
-              sx={{ height: { xs: "auto", md: "80vh" } }}
-            >
-              <Box sx={{ display: "flex" }}>
-                <Typography
-                  variant="h1"
-                  align="center"
-                  className="hero-title"
-                  sx={{ width: { xs: "90vw", md: "50vw" } }}
-                >
-                  {"Hi, I am Daniel"}
-                </Typography>
-              </Box>
-              <div>
-                <Typography
-                  variant="h6"
-                  align="center"
-                  className="description"
-                  sx={{ width: { xs: "90vw", md: "50vw" } }}
-                >
-                  Software Engineer with experience in web, frontend, backend
-                  and game development.
-                </Typography>
-              </div>
-            </Stack>
-          </Stack>
-          <div style={{ display: "flex", justifyContent: "center" }}>
-            <SvgIcon
-              component={KeyboardDoubleArrowDownIcon}
-              className="scroll-indicator"
-              sx={{
-                width: "10vh",
-                height: "20vh",
-                p: "1vh",
-              }}
-            />
-          </div>
-        </div>
-      </Box>
+      <Hero />
       <Box
         id="projects"
         className="section projects-section"
